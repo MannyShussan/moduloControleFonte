47077e41d3008f5c6ed26cb7076a48f221ccadba
Flávio Miranda <nascimentoflavio588@hotmail.com>
2024-05-06 12:52:56 -0300

    Primeiro commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=moduloControleFonte.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/moduloControleFonte.X.production.hex
